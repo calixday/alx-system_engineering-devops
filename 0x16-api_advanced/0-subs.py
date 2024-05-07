48c3d92e87799175eee804e5ab267083284f5e8e
KOECH AMOS <142664159+calixday@users.noreply.github.com>
2024-05-07 13:22:13 +0300

    1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function number of subscribers.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    users = {"User-Agent": "Amoh/5.0"}
+    response = requests.get(url, headers=users, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
