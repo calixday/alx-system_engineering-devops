f8fc71a4fb8a8a78259c39104203ef3032b6ae86
calixday <amoskipkorir1325@gmail.com>
2024-05-08 13:01:14 +0300

    puppet applied

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,9 +2,22 @@
 
 $file_to_edit = '/var/www/html/wp-settings.php'
 
-#replace line containing "phpp" with "php"
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+# Ensure the directory exists
+file { '/var/www/html':
+  ensure => directory,
 }
+
+# Ensure the file exists
+file { $file_to_edit:
+  ensure => file,
+  mode   => '0644',
+}
+
+# Replace line containing "phpp" with "php"
+exec { 'replace_line':
+  command     => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path        => ['/bin','/usr/bin'],
+  onlyif      => "test -f ${file_to_edit}",
+  refreshonly => true,
+}
+
